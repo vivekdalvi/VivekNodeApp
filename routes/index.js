33d6e4b3d6361bb23ef6100afba7ae49e603e968
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-05 11:37:30 -0800

    added about me page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log("This responds to home page");
   res.render('index', { title: 'CodingBuddy' });
 });
+/* Get About Me page */
+router.get('/aboutme', function (req, res, next) {
+  console.log("This responds to aboutme page");
+  res.render('aboutme', { title: 'CodingBuddy' });
+});
 
+router.get('/test', function (req, res, next) {
+  console.log("This responds to test page");
+  res.send("Test");
+});
 module.exports = router;
