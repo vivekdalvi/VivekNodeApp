4007db349c20175f861adc255c47beed384813c4
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-07 11:40:16 -0800

    feedback post form

--- routes/aboutme.js
+++ routes/aboutme.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next) {
     res.render('aboutme', { title: 'CodingBuddy' });
 });
 
+router.get('/feedback', function (req, res, next) {
+    console.log("This responds to feedback logic");
+    res.render('aboutme', { title: 'CodingBuddy' });
+});
+
 module.exports = router;
