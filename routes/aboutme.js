36ea4836319ba3a621724f2feaa018ad1196965b
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-22 09:47:22 -0800

    Basics of Post

--- routes/aboutme.js
+++ routes/aboutme.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+
 router.get('/', function (req, res, next) {
     console.log("This responds to aboutme page (Vihan)");
     res.render('aboutme', { title: 'CodingBuddy' });
@@ -10,6 +15,13 @@ router.get('/feedback', function (req, res, next) {
     console.log("This responds to feedback logic");
     //res.render('aboutme', { title: 'CodingBuddy' });
     res.send('feedback received')
+});
+
+router.post('/feedback', function (req, res, next) {
+    console.log("This responds to feedback logic");
+    console.log(req.body.comment);
+    //res.render('aboutme', { title: 'CodingBuddy' });
+    res.send('feedback received from');
 });
 
 module.exports = router;
