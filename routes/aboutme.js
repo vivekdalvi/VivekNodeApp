9f4d9a7d543842b1f4dac4b3e81b6f0db61f30ab
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-24 13:26:06 -0800

    deleted commented out code

--- routes/aboutme.js
+++ routes/aboutme.js
@@ -14,17 +14,14 @@ router.get('/', function (req, res, next) {
 
 router.get('/feedback', function (req, res, next) {
     console.log("This responds to feedback logic");
-    //res.render('aboutme', { title: 'CodingBuddy' });
     res.send('feedback received')
 });
 
 router.post('/feedback', function (req, res, next) {
     console.log("This responds to feedback logic");
     console.log(req.body.comment);
-    //res.render('aboutme', { title: 'CodingBuddy' });
     var feedbackreceived = "Feedback received from " + req.body.name;
     const feedback = new Feedback(req.body.name, req.body.email, req.body.comment);
-    // var feedback = new Feedback(req.body.name, req.body.email, req.body.comment);
     res.send("Thank you for your feedback " + feedback.getUserName());
 });
 
