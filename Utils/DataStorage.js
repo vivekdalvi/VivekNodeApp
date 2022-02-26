a85cd1e467105530ad7398f7c865d7caf77335d1
Vivek Dalvi <vivekdalvi@hotmail.com>
2022-02-26 07:17:56 -0800

    feedback writing into file

---
+++ Utils/DataStorage.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+
+class DataStorage {
+
+    constructor(filepath) {
+        this.FilePath = filepath;
+    }
+
+    async ReadFile() {
+        try {
+            const data = await fs.promises.readFile(this.FilePath, { encoding: 'utf-8', });
+            //console.log(data);
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async WriteFile(entry) {
+        try {
+            await fs.promises.appendFile(this.FilePath, entry + "\n");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+module.exports = DataStorage;
--- routes/aboutme.js
+++ routes/aboutme.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const Feedback = require('../Utils/Feedback');
-
+const DataStorage = require('../Utils/DataStorage');
 
 var router = express.Router();
 router.use(express.urlencoded({ extended: true }));
@@ -19,9 +19,12 @@ router.get('/feedback', function (req, res, next) {
 
 router.post('/feedback', function (req, res, next) {
     console.log("This responds to feedback logic");
-    console.log(req.body.comment);
+    //console.log(req.body.comment);
     var feedbackreceived = "Feedback received from " + req.body.name;
     const feedback = new Feedback(req.body.name, req.body.email, req.body.comment);
+    const datastorage = new DataStorage('./Utils/feedback.txt');
+    const entry = datastorage.WriteFile(feedback.getUserName() + "," + feedback.getEmail() + "," + feedback.getComment());
+    const data = datastorage.ReadFile().then(console.log);
     res.send("Thank you for your feedback " + feedback.getUserName());
 });
 
